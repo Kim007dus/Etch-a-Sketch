0af4f21cea043103e4adc505f6c29d2786fe02d7
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-23 17:11:44 +0100

    last version not workin grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
 const container = document.querySelector("#container")
 const grid = document.createElement("div")
 grid.classList.add("grid")
-
-grid.style.width = "600px"
-grid.style.height = "600px"
-grid.style.background = "red"
-grid.style.color = "black"
-
 container.appendChild(grid)
 
-const table = document.createElement ("table")
-table.classList.add ("table")
-table.style.width = "400px"
-table.style.height = "400px"
-table.style.background = "white"
+let numDivs = 16
 
-grid.appendChild(table)
+function makeDivs (numDivs) {
+for (let i=0; i > numDivs; i++) {
+        let cells = document.createElement("div")
+        cells.classList.add (cells)
+        grid.appendChild(cells)
+    }
+}
+
+
+
+
+
+
+
+
