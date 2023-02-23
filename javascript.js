0a7bca6807ccadd59f84ad3a0e2b1cba4e2d0f7e
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-23 15:33:55 +0100

    add table element

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
-const container = document.querySelector ("#container")
-const grid = document.createElement ("div")
-grid.classList.add ("grid") 
+const container = document.querySelector("#container")
+const grid = document.createElement("div")
+grid.classList.add("grid")
 
-grid.style.width = "550px"
-grid.style.height = "550px"
+grid.style.width = "600px"
+grid.style.height = "600px"
 grid.style.background = "red"
 grid.style.color = "black"
 
 container.appendChild(grid)
 
-console.log (grid)
+const table = document.createElement ("table")
+table.classList.add ("table")
+table.style.width = "400px"
+table.style.height = "400px"
+table.style.background = "white"
 
+grid.appendChild(table)
