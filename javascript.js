34b21fe882a4978eae5c56a652d8b09b284462ec
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-28 14:01:58 +0100

    shows a grid with too much space between rows

--- javascript.js
+++ javascript.js
@@ -1,17 +1,13 @@
 const container = document.querySelector("#container")
-const grid = document.createElement("div")
-grid.classList.add("grid")
-container.appendChild(grid)
 
-let numDivs = 16
 
-function makeDivs (numDivs) {
-for (let i=0; i > numDivs; i++) {
-        let cells = document.createElement("div")
-        cells.classList.add (cells)
-        grid.appendChild(cells)
+
+
+for (let i = 1; i < 257; i++) {
+        const div = document.createElement("div")
+        container.appendChild(div)
     }
-}
+
 
 
 
