fecd5be0599af6ffe1198f58e0f224dd959572c3
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-28 17:34:03 +0100

    add grid 16x16

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
 
 const container = document.querySelector("#container")
+const grid = document.createElement ("div")
+grid.classList.add ("grid")
+container.appendChild(grid)
 
-function makeDivs (numDivs) {
-        for (let c = 0; c < (numDivs * numDivs); c++) {
-        let cells = document.createElement ("div")
-        container.appendChild(cells)
+
+function makeDivs (row, col) {
+    grid.style.setProperty ('--grid-col', col)
+    grid.style.setProperty ('--grid-row', row)
+        for (let c = 0; c < (col * row); c++) {
+        let cell = document.createElement ("div")
+        grid.appendChild(cell)
+
+       
+        
         
     }
 
 }
 
-makeDivs (16)
+makeDivs (16, 16)
+
+
 
 
 
