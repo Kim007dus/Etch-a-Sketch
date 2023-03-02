9199fc45854babae105d79f236f12373c95e61d4
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-03-02 12:06:14 +0100

    add button to choose size grid

--- javascript.js
+++ javascript.js
@@ -1,31 +1,35 @@
-
 const container = document.querySelector("#container")
-const grid = document.createElement ("div")
-grid.classList.add ("grid")
+const grid = document.createElement("div")
+grid.classList.add("grid")
 container.appendChild(grid)
 
 
-function makeDivs (row, col) {
-    grid.style.setProperty ('--grid-col', col)
-    grid.style.setProperty ('--grid-row', row)
-    
-        for (c = 0; c < (col * row); c++) {
-        let cell = document.createElement ("div")
-        cell.classList.add ("cell")
-        grid.appendChild(cell) 
+function makeDivs(number) {
+    for (c = 0; c < (number * number); c++) {
+        let cell = document.createElement("div")
+        cell.classList.add("cell")
+        grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`
+        grid.style.gridTemplateRows = `repeat(${number}, 1fr)`
+        grid.appendChild(cell)
         cell.addEventListener("mousemove", () => {
-            cell.classList.add ("sketching")
+            cell.classList.add("sketching")
         })
     }
-   
+
 }
 
-makeDivs (16, 16)
+makeDivs(16)
+
+const buttonStart = document.querySelector("#start")
+
+buttonStart.addEventListener("click", () => {
+    let row = prompt("Choose the size of the sketch please! (max 100)")
+    if (row > 100) return alert("Error, the size needs to be smaller than 100!")
+    if (row <= 100) {
+        makeDivs(row)
+    }
+
+})
+
 
 // The cell needs to change color when hover and stay that color.
-
-
-
-
-
-
